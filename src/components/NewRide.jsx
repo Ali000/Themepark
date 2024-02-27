7c84a06c16344bd124a4f823004b9cae1308cac8
Ali Albanna <ali.al-banna@outlook.com>
2024-02-27 12:45:22 +0300

    edit ride form to match DB schema

--- src/components/NewRide.jsx
+++ src/components/NewRide.jsx
@@ -5,6 +5,8 @@ const NewRide = () => {
   const nameRef = useRef(null);
   const imageRef = useRef(null);
   const thrillRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const ageRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,7 +14,9 @@ const NewRide = () => {
     let formFields = {
       name: nameRef.current.value,
       image: imageRef.current.value,
-      thrill: thrillRef.current.value
+      thrill: thrillRef.current.value,
+      ageLimit: ageRef.current.value,
+      description: descriptionRef.current.value
     }
     console.log(formFields)
   }
@@ -24,6 +28,10 @@ const NewRide = () => {
       <input ref={nameRef} type="text" id="rideName"/>
       <label htmlFor="rideImage">Image</label>
       <input ref={imageRef} type="text" id="rideImage"/>
+      <label htmlFor="ageLimit">Age Limit</label>
+      <input ref={ageRef} type="number" id="ageLimit"/>
+      <label htmlFor="rideDesc">Description</label>
+      <textarea ref={descriptionRef} id="rideDesc" cols="30" rows="10"></textarea>
       <label htmlFor="thrillLevel">Thrill Level</label>
       <select ref={thrillRef} name="" id="thrilLevel">
         <option value="Calm">Calm</option>
@@ -35,7 +43,6 @@ const NewRide = () => {
       <button type="submit">Add ride</button>
     </form>
     </div>
-
   )
 
 }
