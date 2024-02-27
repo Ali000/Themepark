47c823a340ea0a6413169167b97b6ad0ecbf7f22
fatima <fatimafouad9999@gmail.com>
2024-02-27 15:56:55 +0300

    added delete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,45 +4,51 @@ import { Link } from "react-router-dom"
 import axios from "axios"
 
 const Home = () => {
+  const [parks, setParks] = useState([])
 
-    const [parks, setParks] = useState([]);
+  const getParks = async () => {
+    let response = await axios.get("http://localhost:3000/themeParks")
+    setParks(response.data)
+  }
+  const deleteThemePark = async (id) => {
+    await axios.delete(`http://localhost:3000/themeParks/${id}`)
+    getParks()
+  }
 
-    const getParks = async () => {
-        let response = await axios.get("http://localhost:3000/themeParks");
-        setParks(response.data);
-    }
+  useEffect(() => {
+    getParks()
+  }, [])
 
-    useEffect(() => {
-        getParks();
-    }, [])
-
-    return (
-        <div>
-            <Nav />
-            <div className="wrapper">
-            {
-                parks.map((park) => {
-                    return (
-                        <div key={park._id} className="themepark-card">
-                            <div  className="themepark-details">
-                                <div className="themepark-image">
-                                    <img src={park.image} alt={park.name} />
-                                </div>
-                                <div className="themepark-name">
-                                    <h3>{park.name}</h3>
-                                    <p>{park.description}</p>
-                                </div>
-                            </div>
-                            <div className="themepark-view-more">
-                                <Link to={"/themepark/" + park._id} state={{id: park._id}}><button>View More</button></Link>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+  return (
+    <div>
+      <Nav />
+      <div className="wrapper">
+        {parks.map((park) => {
+          return (
+            <div key={park._id} className="themepark-card">
+              <div className="themepark-details">
+                <div className="themepark-image">
+                  <img src={park.image} alt={park.name} />
+                </div>
+                <div className="themepark-name">
+                  <h3>{park.name}</h3>
+                  <p>{park.description}</p>
+                </div>
+              </div>
+              <div className="themepark-delete">
+                <button onClick={() =>deleteThemePark(park._id)}>Delete</button>
+              </div>
+              <div className="themepark-view-more">
+                <Link to={"/themepark/" + park._id} state={{ id: park._id }}>
+                  <button>View More</button>
+                </Link>
+              </div>
             </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
